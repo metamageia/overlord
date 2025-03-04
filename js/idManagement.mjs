7be238795ee447e2543ba100ea2799e0ed3dcb22
“Metamageia” <metamageia@gmail.com>
2025-03-03 23:35:45 -0600

    Refactor: idManagement

---
+++ js/idManagement.mjs
@@ -0,0 +1,50 @@
+/* ---------------------------------------------
+ * STATBLOCKID GENERATION CODE
+ * ---------------------------------------------
+ */
+function hashString(str) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = (hash << 5) - hash + str.charCodeAt(i);
+    hash |= 0;
+  }
+  return ("00000000" + (hash >>> 0).toString(16)).slice(-8);
+}
+function canonicalize(obj) {
+  if (Array.isArray(obj)) {
+    return obj.map(item => canonicalize(item));
+  } else if (obj && typeof obj === "object" && !(obj instanceof Date)) {
+    const sortedKeys = Object.keys(obj).sort();
+    const newObj = {};
+    for (let key of sortedKeys) {
+      newObj[key] = canonicalize(obj[key]);
+    }
+    return newObj;
+  }
+  return obj;
+}
+
+export function generateStatblockID(statblockObj) {
+  const copy = JSON.parse(JSON.stringify(statblockObj));
+  delete copy.statblockID;
+  delete copy.bundleId;
+  const canonicalObj = canonicalize(copy);
+  const str = JSON.stringify(canonicalObj);
+  return hashString(str);
+}
+export function generateBundleID(bundleArray) {
+  const clones = bundleArray.map(sb => {
+    let copy = JSON.parse(JSON.stringify(sb));
+    delete copy.statblockID;
+    delete copy.bundleId;
+    return copy;
+  });
+  clones.sort((a,b) => {
+    const A = (a.monsterName || "").toLowerCase();
+    const B = (b.monsterName || "").toLowerCase();
+    return A.localeCompare(B);
+  });
+  const canonical = canonicalize(clones);
+  const str = JSON.stringify(canonical);
+  return hashString(str);
+}
--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import { masterYamlData, updateMasterYamlData, resetMasterYamlData, hiddenStats, DEFAULT_STATS } from "./js/yamlDataState.mjs";
 import { updateYamlTextArea, updateMasterYamlDataFromYaml, updateUIFromMasterYaml, updateMasterYamlDataFromUI, uiFieldChanged } from "./js/masterYamlData.mjs";
-import { parseDeedsStringNew, } from "./js/utilityFunctions.mjs";
 import { updateRenderedStatblock, renderDefaultDetail } from "./js/statblockRender.mjs";
 import { statblocks, uploadedBundles, favoritesMap, loadFromLocalStorage, saveToLocalStorage, loadUploadedBundles, saveUploadedBundles, exportBackup, importBackup, downloadBlob, clearLocalStorage, initSearch } from "./js/libraryData.mjs";
+import { generateStatblockID, generateBundleID } from "./js/idManagement.mjs";
 
 /************************************************
  * Global Variables
@@ -128,58 +128,7 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-/* ---------------------------------------------
- * STATBLOCKID GENERATION CODE
- * ---------------------------------------------
- */
-function hashString(str) {
-  let hash = 0;
-  for (let i = 0; i < str.length; i++) {
-    hash = (hash << 5) - hash + str.charCodeAt(i);
-    hash |= 0;
-  }
-  return ("00000000" + (hash >>> 0).toString(16)).slice(-8);
-}
 
-function canonicalize(obj) {
-  if (Array.isArray(obj)) {
-    return obj.map(item => canonicalize(item));
-  } else if (obj && typeof obj === "object" && !(obj instanceof Date)) {
-    const sortedKeys = Object.keys(obj).sort();
-    const newObj = {};
-    for (let key of sortedKeys) {
-      newObj[key] = canonicalize(obj[key]);
-    }
-    return newObj;
-  }
-  return obj;
-}
-
-function generateStatblockID(statblockObj) {
-  const copy = JSON.parse(JSON.stringify(statblockObj));
-  delete copy.statblockID;
-  delete copy.bundleId;
-  const canonicalObj = canonicalize(copy);
-  const str = JSON.stringify(canonicalObj);
-  return hashString(str);
-}
-
-function generateBundleID(bundleArray) {
-  const clones = bundleArray.map(sb => {
-    let copy = JSON.parse(JSON.stringify(sb));
-    delete copy.statblockID;
-    delete copy.bundleId;
-    return copy;
-  });
-  clones.sort((a,b) => {
-    const A = (a.monsterName || "").toLowerCase();
-    const B = (b.monsterName || "").toLowerCase();
-    return A.localeCompare(B);
-  });
-  const canonical = canonicalize(clones);
-  const str = JSON.stringify(canonical);
-  return hashString(str);
-}
 
 /* ---------------------------------------------
  Statblock Export and Sharing
@@ -495,7 +444,6 @@ function renderUploadedBundles(){
   table.appendChild(tbody);
   container.appendChild(table);
 }
-
 
 
 /* ---------------------------------------------
@@ -1575,7 +1523,6 @@ function attachEventHandlers() {
       selectedStatblockID = null;
       updateRenderedStatblock();
       updateUIFromMasterYaml();
-      document.getElementById("yamlInput").value = "";
     }
   });
   document.getElementById("exportBackupBtn").addEventListener("click", exportBackup);
